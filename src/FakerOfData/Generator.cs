d3e91ff1d577e8175e3b1b736715a24590fbebd3
David Tellander <dtellander@gmail.com>
2014-06-05 19:14:07 +0200

    Add Of-method that generate a sequence of objects
    The of method returns an inifinte sequence of the type specified as the
    type argument. The caller can specify zero or more action functions that
    is passed a new instance of the type.

---
+++ src/FakerOfData/Generator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FakerOfData {
+    public static class Generator {
+        public static IEnumerable<T> Of<T>(params Action<T>[] setters) where T : new() {
+            while (true) { yield return setters.Aggregate(new T(), (i, a) => { a(i); return i; }); }
+        }
+    }
+}
---
+++ test/FakerOfData.Test/GeneratorTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using NFluent;
+using Xunit;
+
+namespace FakerOfData.Test {
+    public class GeneratorTest {
+
+        public class Of_method {
+
+            [Fact]
+            public void should_generate_a_sequence_of_the_specified_type() {
+                var persons = Generator.Of<Person>()
+                    .Take(3)
+                    .ToList();
+
+                Check.That(persons).HasSize(3);
+            }
+
+            [Fact]
+            public void should_call_each_action_specified_as_arguments() {
+                var date = DateTime.Now;
+                var name = "John";
+
+                var person = Generator.Of<Person>(
+                        p => p.Name = name,
+                        p => p.BirthDate = date
+                    )
+                    .First();
+
+                Check.That(person).HasFieldsWithSameValues(new {Name = name, BirthDate = date});
+            }
+        }
+    }
+
+    public class Person {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
