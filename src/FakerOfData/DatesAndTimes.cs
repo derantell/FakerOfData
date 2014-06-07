8aa42ec9c57cc8ad4f413654f9d2cf38c9025b60
David Tellander <dtellander@gmail.com>
2014-06-07 16:16:44 +0200

    Add methods that helps getting random dates
    Extension methods Date and DateBetween of Random which return random
    dates.

---
+++ src/FakerOfData/DatesAndTimes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace FakerOfData {
+    public static class DatesAndTimes {
+        public static DateTime Date(this Random self) {
+            return new DateTime( (long) (self.NextDouble() * DateTime.MaxValue.Ticks));
+        }
+
+        public static DateTime DateBetween(this Random self, DateTime? from = null, DateTime? to = null) {
+            from = from ?? DateTime.MinValue;
+            to   = to   ?? DateTime.MaxValue;
+
+            var span = to.Value.Ticks - from.Value.Ticks;
+            return new DateTime(from.Value.Ticks + (long)(span * self.NextDouble()));
+        }
+
+        public static Func<DateTime, int, DateTime> Years(this int self) {
+            return (d, t) => d.AddYears(t*self);
+        }
+
+        public static Func<DateTime, int, DateTime> Months(this int self) {
+            return (d, t) => (t == TimeDirection.Past) ? d.AddMonths(-self) : d.AddMonths(self);
+        }
+
+        public static Func<DateTime, int, DateTime> Days(this int self) {
+            return (d, t) => (t == TimeDirection.Past) ? d.AddDays(-self) : d.AddDays(self);
+        }
+
+        public static DateTime Ago(this Func<DateTime, int, DateTime> self) {
+            return self(DateTime.Now, TimeDirection.Past);
+        }
+
+        public static DateTime FromNow(this Func<DateTime, int, DateTime> self) {
+            return self(DateTime.Now, TimeDirection.Future);
+        }
+
+        public static DateTime Before(this Func<DateTime, int, DateTime> self, DateTime date) {
+            return self(date, TimeDirection.Past);
+        }
+
+        public static DateTime After(this Func<DateTime, int, DateTime> self, DateTime date) {
+            return self(date, TimeDirection.Future);
+        }
+    }
+
+    public struct TimeDirection {
+        public const int Past = -1;
+        public const int Future = 1;
+    }
+}
---
+++ test/FakerOfData.Test/DatesAndTimesTest.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using NFluent;
+using Xunit;
+
+namespace FakerOfData.Test {
+    public class DatesAndTimesTest {
+
+        public class Date_method {
+
+            [Fact]
+            public void should_return_a_random_datetime_object() {
+                var random = new Random();
+
+                var date1 = random.Date();
+                var date2 = random.Date();
+
+                Check.That(date1).Not.IsEqualTo(date2);
+            }
+        }
+
+        public class DateBetween_method {
+            [Fact]
+            public void should_return_a_date_between_the_two_specified_dates() {
+                var random = new Random();
+
+                var start = new DateTime(1976, 1, 2);
+                var end = new DateTime(1976, 1, 3);
+
+                var theDate = random.DateBetween(start, end);
+
+                Check.That(theDate).IsAfter(start).And.IsBefore(end);
+            }
+        }
+
+        public class Years_method {
+
+            [Fact]
+            public void should_return_function_that_subtracts_years_from_specified_date_when_direction_is_past() {
+                var years = 2.Years();
+                var date = new DateTime(1976, 1, 1);
+                var twoYearsPast = years(date, TimeDirection.Past);
+                Check.That(twoYearsPast.Year).IsEqualTo(1974);
+            }
+
+            [Fact]
+            public void should_return_a_function_that_adds_the_years_to_specified_date_when_direction_is_future() {
+                var years = 2.Years();
+                var date = new DateTime(1976, 1, 1);
+                var twoYearsFuture = years(date, TimeDirection.Future);
+                Check.That(twoYearsFuture.Year).IsEqualTo(1978);
+            }
+        }
+
+        public class Months_method{
+            [Fact]
+            public void should_return_a_function_that_subtracts_months_from_specified_date_when_direction_is_past() {
+                var months = 2.Months();
+                var date = new DateTime(1976, 1, 1);
+                var twoMonthsPast = months(date, TimeDirection.Past);
+                Check.That(twoMonthsPast.Month).IsEqualTo(11);
+            }
+
+            [Fact]
+            public void should_return_a_function_that_adds_months_from_specified_date_when_direction_is_past() {
+                var months = 2.Months();
+                var date = new DateTime(1976, 1, 1);
+                var twoMonthsFuture = months(date, TimeDirection.Future);
+                Check.That(twoMonthsFuture.Month).IsEqualTo(3);
+            }
+        }
+
+        public class Days_method {
+            [Fact]
+            public void should_return_a_function_that_subtracts_days_from_specified_date_when_direction_is_past() {
+                var days = 2.Days();
+                var date = new DateTime(1976, 1, 1);
+                var twoDaysPast = days(date, TimeDirection.Past);
+                Check.That(twoDaysPast.Day).IsEqualTo(30);
+            }
+
+            [Fact]
+            public void should_return_a_function_that_adds_days_from_specified_date_when_direction_is_past() {
+                var days = 2.Days();
+                var date = new DateTime(1976, 1, 1);
+                var twoDaysFuture = days(date, TimeDirection.Future);
+                Check.That(twoDaysFuture.Day).IsEqualTo(3);
+            }
+        }
+
+        public class Ago_method {
+            [Fact]
+            public void should_subract_time_from_a_date() {
+                var actualDirection = TimeDirection.Future;
+                Func<DateTime, int, DateTime> function =
+                    (d, t) => { actualDirection = t; return d; };
+
+                function.Ago();
+
+                Check.That(actualDirection).IsEqualTo(TimeDirection.Past);
+            }
+        }
+
+        public class FromNow_method {
+            [Fact]
+            public void should_add_time_to_a_date() {
+                var actualDirection = TimeDirection.Past;
+                Func<DateTime, int, DateTime> function =
+                    (d, t) => { actualDirection = t; return d; };
+
+                function.FromNow();
+
+                Check.That(actualDirection).IsEqualTo(TimeDirection.Future);
+            }
+        }
+
+        public class Before_method {
+            [Fact]
+            public void should_subtract_time_from_the_specified_date() {
+                var actualDirection = TimeDirection.Future;
+                var referenceDate = new DateTime(1976, 1, 1);
+                
+                Func<DateTime, int, DateTime> function =
+                    (d, t) => { actualDirection = t; return d; };
+
+                var actualDate = function.Before(referenceDate);
+
+                Check.That(actualDirection).IsEqualTo(TimeDirection.Past);
+                Check.That(actualDate).IsEqualTo(referenceDate);
+            }
+        }
+
+        public class After_method {
+            [Fact]
+            public void should_subtract_time_from_the_specified_date() {
+                var actualDirection = TimeDirection.Past;
+                var referenceDate = new DateTime(1976, 1, 1);
+
+                Func<DateTime, int, DateTime> function =
+                    (d, t) => { actualDirection = t; return d; };
+
+                var actualDate = function.After(referenceDate);
+
+                Check.That(actualDirection).IsEqualTo(TimeDirection.Future);
+                Check.That(actualDate).IsEqualTo(referenceDate);
+            }
+        }
+    }
+}
