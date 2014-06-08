2e3c9dfc43fb5ee2240fd12e2b549e7c8a6a22d8
David Tellander <dtellander@gmail.com>
2014-06-08 10:30:14 +0200

    Add console app to test the API

---
+++ test/FakerOfData.TestApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+
+namespace FakerOfData.TestApp {
+    class TheTestData {
+        public int Index { get; set; }
+        public string TimeStamp { get; set; }
+        public Guid UniqueId { get; set; }
+        public string Category { get; set; }
+    }
+
+    static class Ext {
+        public static string Category(this Random self) {
+            IEnumerable<string> strings = Generator.Strings.Category;
+            return strings.ElementAt(self.Next(strings.Count()));
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Generator.Destination = new CsvDestination("\t", s => new StreamWriter(@"c:\slask\" + s));
+            Generator.Strings = new DynamicStringCollection(new FileStringSource("Strings"));
+
+            Generator.Of<TheTestData>(
+                d => d.Index = Counter.Next.Index,
+                d => d.TimeStamp = Some.Random.DateBetween(2.Years().Ago(), 1.Years().FromNow()).ToString("s"),
+                d => d.UniqueId = Guid.NewGuid(),
+                d => d.Category = Some.Random.Category())
+
+                .Take(1000)
+                .Load();
+        }
+    }
+}
