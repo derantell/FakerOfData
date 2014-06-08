19b2b17a459323d4b78a7453c2d63020b3fd8231
David Tellander <dtellander@gmail.com>
2014-06-08 14:20:41 +0200

    Add Sql destination
    The sql destination loads the generated fake data into an SQL database.

---
+++ src/FakerOfData.Sql/SqlDestination.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Simple.Data;
+
+namespace FakerOfData.Sql {
+    public class SqlDestination : IDestination {
+        public SqlDestination(string connectionString) {
+            _connectionString = connectionString;
+            _db = Database.OpenConnection(_connectionString);
+        }
+
+        public void Load<T>(IEnumerable<T> sequence) {
+            var tableName = typeof (T).Name;
+            _db[tableName].Insert(sequence);
+        }
+
+        private readonly dynamic _db;
+        private readonly string _connectionString;
+    }
+}
---
+++ test/FakerOfData.Sql.Test/SqlDestinationTest.cs
@@ -0,0 +1,24 @@
+﻿using NFluent;
+using Xunit;
+
+namespace FakerOfData.Sql.Test {
+    public class SqlDestinationTest {
+
+        public class Load_method {
+
+            [Fact]
+            public void should_insert_the_specified_collection_into_the_db() {
+                var destination = new SqlDestination("foo");
+
+                destination.Load(new[]{new Record {Id = 42}});
+
+                Check.That(destination);
+            }
+
+        }
+
+        public class Record {
+            public int Id { get; set; }
+        }
+    }
+}
