51f8642dd18c10a33621779db2e26bddddaab6cf
David Tellander <dtellander@gmail.com>
2014-06-21 20:53:20 +0200

    Add build directory for build scripts
    Adding the install.ps1 script to see if that works for multiline
    powershell.

---
+++ build/install.ps1
@@ -0,0 +1,5 @@
+# Create a build tag, formatted like {build}.{branch}.{sha7}
+# Use aver to patch versions of assemblies and nupkg-packages
+$sha = "$env:APPVEYOR_REPO_COMMIT".substring(0,7)
+$env:MY_BUILD_TAG = "$env:APPVEYOR_BUILD_NUMBER.$env:APPVEYOR_REPO_BRANCH.$sha"
+& ..\tools\aver.exe set "$($env:APPVEYOR_BUILD_FOLDER)" -scan -build "$env:MY_BUILD_TAG" 
