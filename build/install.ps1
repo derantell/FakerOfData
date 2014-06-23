09f30ba2a42a60d524db4615283bec77c9461769
David Tellander <dtellander@gmail.com>
2014-06-23 21:50:02 +0200

    Set AppVeyor version build detail in install step
    The appveyor build version should look like:
    
    Major.Minor.Patch-Prerelease+BuildNo.Branch.Sha
    
    The version part should be read from the root verson.txt file if that
    exists. The build tag should be the created from environment data provided
    by AppVeyor.

--- build/install.ps1
+++ build/install.ps1
@@ -1,5 +1,14 @@
 # Create a build tag, formatted like {build}.{branch}.{sha7}
-# Use aver to patch versions of assemblies and nupkg-packages
 $sha = "$env:APPVEYOR_REPO_COMMIT".substring(0,7)
 $env:MY_BUILD_TAG = "$env:APPVEYOR_BUILD_NUMBER.$env:APPVEYOR_REPO_BRANCH.$sha"
+# Use aver to patch versions of assemblies and nupkg-packages
 .\tools\aver.exe set "$($env:APPVEYOR_BUILD_FOLDER)" -scan -build "$env:MY_BUILD_TAG" -verbose
+
+# Set Appveyor build info to look like we want it
+# Use version from root version.txt file 
+if(Test-Path .\version.txt) {
+    # Convention: first line of version.txt is semver version
+    $version = (Get-Content .\version.txt -Head 1).Trim()
+    # Use AppVeyor Powershell API to set version+buildtag
+    Update-AppveyorBuild -Version "$version+$env:MY_BUILD_TAG"
+}
