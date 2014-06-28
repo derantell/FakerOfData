cb1c447a33e7d507f6650e40d065dab64bcea165
David Tellander <dtellander@gmail.com>
2014-06-28 16:29:19 +0200

    Add tests for RandomThings class

---
+++ test/FakerOfData.Test/The_RandomStringValue_class.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using FakeItEasy;
+using NFluent;
+using Xunit;
+
+namespace FakerOfData.Test {
+    public class The_RandomStringValue_class {
+
+        class TestStringSource : IStringSource {
+            public IEnumerable<string> Keys {
+                get { return new[] {"Foo"}; }
+            }
+
+            public IEnumerable<string> GetStrings(string key) {
+                return new[] {"Bar", "Baz", "Quux"};
+            }
+        }
+
+        public class Key_property {
+            [Fact]
+            public void should_return_the_name_of_the_string_collection_from_which_strings_are_drawn() {
+                var value = new RandomStringValue("Foo", new TestStringSource());
+
+                Check.That(value.Key).IsEqualTo("Foo");
+            }
+        }
+
+        public class RandomValues_method {
+            [Fact]
+            public void should_return_a_random_string_from_the_specified_collection() {
+                var values = new RandomStringValue("Foo", new TestStringSource());
+
+                var value = (string) values.RandomValues(new {});
+
+                Check.That(value).Matches(@"^(Bar|Baz|Quux)$");
+            }
+        }
+    }
+}
---
+++ test/FakerOfData.Test/The_RandomThings_dynamic_class.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using FakeItEasy;
+using NFluent;
+using Xunit;
+
+namespace FakerOfData.Test {
+    public class The_RandomThings_dynamic_class {
+
+        public class TryGetMember_overridden_method {
+
+            [Fact]
+            public void should_throw_an_exception_when_accessing_a_non_existing_property() {
+                dynamic randomThings = new RandomThings();
+
+                Check.ThatCode(() => randomThings.Foo).Throws<KeyNotFoundException>();
+            }
+
+            [Fact]
+            public void should_get_a_value_from_the_function_registered_under_the_specified_key() {
+                var randomThings = new RandomThings();
+
+                var result = new {};
+                randomThings.RegisterValues("Test", _ => result);
+
+                dynamic dynamicThings = randomThings;
+
+                Check.That((object) dynamicThings.Test).IsSameReferenceThan(result);
+            }
+        }
+
+        public class TrySetMember_overridden_method {
+            [Fact]
+            public void should_set_the_function_of_the_specified_key_when_called_dynamically() {
+                dynamic randomThings = new RandomThings();
+
+                var result = new {};
+                randomThings.Test = new Func<object, object>(_ => result);
+
+                Check.That((object) randomThings.Test).IsSameReferenceThan(result);
+            }
+
+        }
+    }
+}
