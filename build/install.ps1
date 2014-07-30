a4577cdb0daaa5cd0a5e5544a0532c1c999d8e8b
David Tellander <dtellander@gmail.com>
2014-07-30 19:13:42 +0200

    Use special build label for pr merge builds
    If the build is a pr merge build, we mark it as
    
    pr-{pr issue number}-merge
    
    otherwise the branch name is used in the build label

--- build/install.ps1
+++ build/install.ps1
@@ -1,6 +1,14 @@
+# Mark pr merge builds with a special build label
+$branch = if ($env:APPVEYOR_PULL_REQUEST_NUMBER) {
+    "pr-$env:APPVEYOR_PULL_REQUEST_NUMBER-merge"
+} else {
+    $env:APPVEYOR_REPO_BRANCH
+}
+
 # Create a build tag, formatted like {build}.{branch}.{sha7}
 $sha = "$env:APPVEYOR_REPO_COMMIT".substring(0,7)
-$env:MY_BUILD_TAG = "$env:APPVEYOR_BUILD_NUMBER.$env:APPVEYOR_REPO_BRANCH.$sha"
+$env:MY_BUILD_TAG = "$env:APPVEYOR_BUILD_NUMBER.$branch.$sha"
+
 # Use aver to patch versions of assemblies and nupkg-packages
 .\tools\aver.exe set "$($env:APPVEYOR_BUILD_FOLDER)" -scan -build "$env:MY_BUILD_TAG" -verbose
 
